7779b576e1ea6cba8725a972248651afa0df2acd
Fatima Zahrae Aamak <fatimazahraeaamak@gmail.com>
2026-08-11 20:25:06 +0800

    feat: add repeating main menu

--- main.cpp
+++ main.cpp
@@ -1,35 +1,52 @@
 #include <iostream>
-int main(){
-    std::cout<<"====================================\n"<<" CST Achievement Manager\n"<<"====================================\n";
-    std::cout<<'\n';
-    std::cout<<"Welcome!.\n";
-    std::cout<<" 1. Show all students\n";
-    std::cout<<" 2. Search student by ID\n";
-    std::cout<<" 3. Search student by name\n";
-    std::cout<<" 4. Exit\n";
-    int choice;
-    std::cout<<"Enter Your Choice: ";
-    std::cin>>choice;
-   switch (choice)
+
+int main()
 {
-    case 1:
-        std::cout << "Showing all students...\n";
-        break;
+    bool running = true;
 
-    case 2:
-        std::cout << "Searching student by ID...\n";
-        break;
+    while (running)
+    {
+        std::cout << "====================================\n";
+        std::cout << " CST Achievement Manager\n";
+        std::cout << "====================================\n";
+        std::cout << '\n';
 
-    case 3:
-        std::cout << "Searching student by name...\n";
-        break;
+        std::cout << "Welcome!\n";
+        std::cout << " 1. Show all students\n";
+        std::cout << " 2. Search student by ID\n";
+        std::cout << " 3. Search student by name\n";
+        std::cout << " 4. Exit\n";
 
-    case 4:
-        std::cout << "Exiting program...\n";
-        break;
+        int choice;
 
-    default:
-        std::cout << "Invalid choice.\n";
-}
+        std::cout << "Enter Your Choice: ";
+        std::cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                std::cout << "Showing all students...\n";
+                break;
+
+            case 2:
+                std::cout << "Searching student by ID...\n";
+                break;
+
+            case 3:
+                std::cout << "Searching student by name...\n";
+                break;
+
+            case 4:
+                std::cout << "Exiting program...\n";
+                running = false;
+                break;
+
+            default:
+                std::cout << "Invalid choice.\n";
+        }
+
+        std::cout << '\n';
+    }
+
     return 0;
 }
